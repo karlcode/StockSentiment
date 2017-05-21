883712163610fefa86d19e0fbaddb0036716f967
karlcode <karl.l1@hotmail.com>
2017-05-21 14:28:03 +1000

    NY Times fetch

--- src/components/home/expandable.js
+++ src/components/home/expandable.js
@@ -3,13 +3,8 @@ import style from './style.less';
 import {VictoryLabel, VictoryArea, VictoryTheme, VictoryTooltip, VictoryVoronoiContainer, VictoryAxis, VictoryBrushContainer, VictoryZoomContainer, VictoryCandlestick, VictoryChart, VictoryLine } from 'victory';
 import Autosuggest from 'react-autosuggest';
 import theme from './style.less';
-var data2 = [
-    {x: 3, open: 5, close: 10, high: 15, low: 0},
-    {x: 2, open: 10, close: 15, high: 20, low: 5},
-    {x: 4, open: 15, close: 20, high: 25, low: 10},
-    {x: 1, open: 15, close: 20, high: 40, low: 10}
-  ]
-  var myHeaders = new Headers({
+
+var myHeaders = new Headers({
   "Access-Control-Allow-Origin": "*, *",
 });
 var data = []
@@ -78,7 +73,7 @@ export default class Expand extends Component {
     var data = []
     var min = []
     var max = []
-      var myInit = { method: 'GET',
+    var myInit = { method: 'GET',
                headers: myHeaders,
                mode: 'cors',
                cache: 'force-cache' };
@@ -106,9 +101,14 @@ export default class Expand extends Component {
       }
     }
     this.setState({data:data, min:inf, max:zer})
-  })
-
+    })
+    fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?api_key=08545c90b96a4aafbaba352d72abba71?q=facebook', myInit)
+    .then(res => res.json())
+    .then(response =>{
+      console.log(response)
+    })
   }
+
 	handleZoom(domain) {
     this.setState({selectedDomain: domain});
   }
@@ -194,7 +194,7 @@ export default class Expand extends Component {
         labelComponent={<VictoryTooltip renderInPortal text={(datum)=>"Close: "+datum.close}/>}
         data={this.state.data}
           x="x"
-            y="open" 
+            y="close" 
             
             />
          <VictoryAxis
@@ -219,6 +219,7 @@ export default class Expand extends Component {
         renderSuggestion={renderSuggestion}
         inputProps={inputProps}
       />
+      
          
       {/*<VictoryChart
             padding={{top: 0, left: 65,  bottom: 30}}
---
+++ src/components/home/tickers.js
@@ -0,0 +1,23 @@
+export const tickers = [
+
+  {
+    ticker: 'MSFT',
+    name: 'Microsoft'
+  },
+  {
+    ticker: 'MSM',
+    name: '1972'
+  },
+  {
+    ticker: 'TSLA',
+   name: 'Tesla Motors Inc'
+  },
+  {
+    ticker: 'FB',
+    name: 'Facebook'
+  },
+  {
+    ticker: 'Elm',
+   name: 'Elm'
+  }
+];
